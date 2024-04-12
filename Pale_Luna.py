ed6e363552c951c68fddd3856dbf1a8153c5d4d9
tnich4 <165207040+tnich4@users.noreply.github.com>
2024-04-12 12:11:52 -0500

    Add files via upload

--- Pale_Luna.py
+++ Pale_Luna.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from PIL import Image, ImageTk
+import os
 
 class PaleLunaGUI:
     def __init__(self, master):
@@ -8,6 +9,9 @@ class PaleLunaGUI:
 
         #SET BG TO BLACK
         self.master.config(bg="black")
+
+        #DIRECTORY FOR IMAGES
+        self.images_dir = os.path.join(os.path.dirname(__file__), r"C:\Users\tenic\Downloads\UIUC Spring 2024\BCOG 200\Pale Luna\images")
 
         #REQUIREMENTS FOR LEAVING PSG 1
         self.current_passage = 1
@@ -20,19 +24,19 @@ class PaleLunaGUI:
 
         #DICTIONARY FOR IMAGES USED IN THE GAME. USE ONLY PNG!
         self.images = {
-            1: "dark_room.png",
-            2: "censored.png",
-            3: "rope.png",
-            4: "shovel.png",
-            5: "open_door.png",
-            6: "forest1.png",
-            7: "forest2.png",
-            8: "forest3.png",
-            9: "forest4.png",
-            10: "clearing.png",
-            11: "clearing_hole.png",
-            12: "bury_gold.png",
-            13: "coords.png",
+            0: "dark_room.png",
+            1: "gold.png",
+            2: "rope.png",
+            3: "shovel.png",
+            4: "open_door.png",
+            5: "forest1.png",
+            6: "forest2.png",
+            7: "forest3.png",
+            8: "forest4.png",
+            9: "here.png",
+            10: "hole.png",
+            11: "bury_gold.png",
+            12: "coords.png",
         }
 
         #ALL SCENARIOS IN GAME
@@ -48,7 +52,7 @@ class PaleLunaGUI:
             5: {"text": "You traverse EAST. \nReap your reward. \nPALE LUNA SMILES AT YOU. \nYou are in a forest. There are paths to the NORTH, WEST, AND SOUTH.",
                 "choices": ["Go NORTH", "Go WEST", "Go SOUTH", "Use GOLD", "Use SHOVEL", "Use ROPE"]},
             6: {"text": "Reap your reward. \nPALE LUNA SMILES AT YOU. \nYou are here.", 
-                "choices": ["Use GOLD", "Use SHOVEL", "Use ROPE"]},
+                "choices": ["Use GOLD", "Use ROPE", "Use SHOVEL"]},
         }
 
         #FRAME FOR TEXT AND BUTTONS 
@@ -58,6 +62,12 @@ class PaleLunaGUI:
         # SETS BACKGROUND AND TEXT COLOR
         self.passage_label = tk.Label(self.frame, text="", wraplength=400, bg="black", fg="white", font=("Arial", 16)) 
         self.passage_label.pack(expand=True, fill="both", padx=20, pady=10)
+
+        #IMPLEMENTATION FOR DISPLAYING IMAGES
+        self.image = Image.open(os.path.join(self.images_dir, self.images[0]))
+        self.photo = ImageTk.PhotoImage(self.image)
+        self.image_label = tk.Label(self.frame, image=self.photo, bg='black')
+        self.image_label.pack(expand=True, fill="both", padx=20, pady=10)
 
         # CHOICE BUTTON CONFIG
         self.choice_buttons = []
@@ -83,17 +93,49 @@ class PaleLunaGUI:
             choice_button.pack(pady=5)
             self.choice_buttons.append(choice_button)
 
+        if self.current_passage == 1:
+            self.update_image(self.images[0])
+        elif self.current_passage == 2:
+            self.update_image(self.images[5])
+        elif self.current_passage == 3:
+            self.update_image(self.images[6])
+        elif self.current_passage == 4:
+            self.update_image(self.images[7])
+        elif self.current_passage == 5:
+            self.update_image(self.images[8])
+        elif self.current_passage == 6:
+            self.update_image(self.images[9])
+        else:
+            self.update_image(self.images[self.current_passage])
+
+    def update_image(self, image_name):
+
+        original_image = Image.open(os.path.join(self.images_dir, image_name))
+        
+        desired_width = 300  
+        
+        width_percent = (desired_width / float(original_image.size[0]))
+        desired_height = int((float(original_image.size[1]) * float(width_percent)))
+        
+        resized_image = original_image.resize((desired_width, desired_height), Image.LANCZOS)
+        
+        self.photo = ImageTk.PhotoImage(resized_image)
+        
+        self.image_label.configure(image=self.photo)
+
 
     #EVERY DECISION THE PLAYER MAKES IS DONE HERE 
     def make_choice(self, choice):
 
         #PASSAGE ONE 
         if self.current_passage == 1:
+            self.update_image(self.images[0])
             if choice == 0:
                 if self.items_collected["GOLD"]:
                     self.passage_label.config(text="You already took this.")
                     return
                 self.items_collected["GOLD"] = True
+                self.update_image("gold.png")  #UPDATE TO SHOW GOLD
                 self.display_intermediary_screen("GOLD")
                 return
             elif choice == 1:
@@ -101,18 +143,24 @@ class PaleLunaGUI:
                     self.passage_label.config(text="You already took this.")
                     return
                 self.items_collected["SHOVEL"] = True
+                self.update_image("shovel.png")
                 self.display_intermediary_screen("SHOVEL")
                 return
+
             elif choice == 2:
                 if self.items_collected["ROPE"]:
                     self.passage_label.config(text="You already took this.")
                     return
                 self.items_collected["ROPE"] = True
+                self.update_image("rope.png")
                 self.display_intermediary_screen("ROPE")
                 return
+
             elif choice == 3:
                 self.items_collected["DOOR_OPEN"] = True
+                self.update_image("open_door.png")
                 self.passage_label.config(text="The door is open.")
+
             elif choice == 4:
                 if all(self.items_collected.values()) and self.items_collected["DOOR_OPEN"]:
                     self.current_passage = 2
@@ -120,16 +168,22 @@ class PaleLunaGUI:
                 else:
                     self.passage_label.config(text="Not yet.")
 
+
+
         #PASSAGE TWO 
         elif self.current_passage == 2: 
+
             if choice == 0:
                 self.current_passage = 3
                 self.update_display()
             elif choice == 1 or choice == 2: 
                 exit()
 
+
+
         #PASSAGE THREE 
         elif self.current_passage == 3:
+
             if choice == 0:
                 self.current_passage = 4
                 self.update_display()
@@ -142,8 +196,11 @@ class PaleLunaGUI:
             elif choice == 5:
                 self.passage_label.config(text="You already used this.")
 
+
+
         #PASSAGE FOUR
         elif self.current_passage == 4:
+
             if choice == 0:
                 exit()
             elif choice == 1: 
@@ -158,8 +215,11 @@ class PaleLunaGUI:
             elif choice == 5:
                 self.passage_label.config(text="You already used this.")
 
+
+
         #PASSAGE FIVE
         elif self.current_passage == 5:
+
             if choice == 0:
                 self.current_passage = 6
                 self.update_display()
@@ -172,8 +232,11 @@ class PaleLunaGUI:
             elif choice == 5:
                 self.passage_label.config(text="You already used this.")
 
+
+
         #PASSAGE SIX - CURRENTLY WIP!!!
         elif self.current_passage == 6:
+
             if choice == 0:
                 self.passage_label.config(text="Can't bury treasure without a hole.")
                 return
@@ -193,7 +256,7 @@ def main():
     root = tk.Tk()
     root.title("Pale Luna")
     root.attributes('-fullscreen', True)  
-    app = AdventureGameGUI(root)
+    app = PaleLunaGUI(root)
     root.mainloop()
 
 if __name__ == "__main__":
